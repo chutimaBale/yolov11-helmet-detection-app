6eea82f62d368f19873901f428a5c478e14598f4
chutimaBale <chutima.thd@gmail.com>
2025-09-24 15:41:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,56 @@
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
+import cv2
 import numpy as np
 
-st.title("YOLO Helmet Detection App :)")
+st.title("YOLO Image Detection App :)")
+st.write("**Helmet Detection: -Demo-**")
 
 # Load YOLO model
-# model = YOLO("runs/detect/train73/weights/best.pt")
-model = YOLO("runs/detect/train11/weights/best.pt")
+model = YOLO("weights/best.pt")
+# model = YOLO("runs/detect/train/weights/best.pt")
 
 # Upload image
 uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "jpeg", "png"])
 
+def count_items_dict(items_index_list):
+	items_index_list = np.sort(items_index_list)
+	count_items = {}
+	for item in items_index_list:
+		if model.names[item] not in count_items:
+			count_items[model.names[item]] = 1
+		else:
+			count_items[model.names[item]] += 1
+	return count_items
+
 if uploaded_image is not None:
-  # Show original image
-  st.image(uploaded_image , caption="Uploaded Image", use_container_width=True)
+	# Show original image
+	st.image(uploaded_image, caption="Uploaded Image", width='stretch')
 
-  # Read image and convert to numpy array
-  image = Image.open(uploaded_image)
-  image_np = np.array(image)
+	# Read image
+	image = Image.open(uploaded_image)
 
-  # Run YOLO inference
-  st.info("Running YOLO helmet detection...")
-  results = model.predict(image_np , conf=0.4)
+	# Run YOLO inference
+	st.info("Running YOLO object detection...")
+	results = model.predict(image, conf=0.3)
 
-  # Draw results on image
-  result_image = results[0].plot()
-  st.image(result_image , caption="YOLO Detection Result", use_container_width=True)
-  st.success("Detection completed!")
+	# Draw results on image
+	result_image = results[0].plot()
+	result_image = cv2.cvtColor(result_image, cv2.COLOR_BGR2RGB)  # Convert to RGB
+	st.image(result_image, caption="YOLO Detection Result", width='stretch')
+	st.success("Detection completed!")
 
-  # Extract detection results
-  boxes = results[0].boxes
-  class_ids = boxes.cls.cpu().numpy().astype(int)
-  class_names = [model.names[i] for i in class_ids]
+	# Extract detection results
+	items_index = results[0].boxes.cls.cpu().numpy().astype(int)
 
-  # Count helmet
-  helmet_count = class_names.count("Helmet")
-  st.write(f"Number of people with helmet detected: **{helmet_count}**")
-  without_helmet_count = class_names.count("Without-Helmet")
-  st.write(f"Number of people without helmet detected: **{without_helmet_count}**")
+	# Check object
+	if len(items_index) == 0:
+		st.warning("No objects were detected in the image.")
+	else:
+		# Count item
+		count_items = count_items_dict(items_index)
+
+		# Show result
+		for item, count in count_items.items():
+			st.write(f"Number of *{item}* detected: **{count}**")
